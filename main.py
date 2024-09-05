e3a614ca821deabeaf43dd4c6785d2ab3bc6295e
pichpich13 <quentinpichard@outlook.com>
2024-09-05 20:08:50 +0200

    suppresion userdashboard

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import subprocess
-
-# Download requirements
-subprocess.call(["pip", "install", "-r", "requirements.txt"])
-
-# Start the dashboard
-subprocess.call(["streamlit", "run", "dashboard.py"])
